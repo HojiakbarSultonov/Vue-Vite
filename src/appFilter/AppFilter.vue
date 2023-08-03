3776839134f35094c995975a0377e6441c7f1af9
HojiakbarSultonov <hojiakbarrasulovich@gmail.com>
2023-08-03 22:46:37 +0500

    btns

--- src/appFilter/AppFilter.vue
+++ src/appFilter/AppFilter.vue
@@ -1,33 +1,48 @@
 <template >
     <div class="btn-group">
-        <button class="btn" @click="filterHandler('all')" :class="[filterName === 'all' ? 'btn-dark' : 'btn-btn-outline-dark']">Barcha kinolar</button>
-        <button class="btn" @click="filterHandler('popular')" :class="[filterName === 'popular' ? 'btn-dark' : 'btn-btn-outline-dark']">Mashhur kinolar</button>
-        <button class="btn" @click="filterHandler('mostViewers')" :class="[filterName === 'mostViewers' ? 'btn-dark' : 'btn-btn-outline-dark']">Eng ko'p ko'rilgan kinolar</button>
+
+        <button v-for="btn in filterButtons" :key="btn.name" class="btn"
+            :class="[filterName === btn.name ? 'btn-dark' : 'btn-outline-dark']" @click="filterHandler(btn.name)">{{ btn.title
+            }}</button>
     </div>
 </template>
 <script>
 export default {
-   props:{
-    updateFilterHandler:{
-        type:Function,
-        required:true
+    props: {
+        updateFilterHandler: {
+            type: Function,
+            required: true
+        },
+        filterName: {
+            type: String,
+            required: true
+        }
     },
-    filterName:{
-        type:String,
-        required:true
+    data() {
+        return {
+            filterButtons: [
+                {
+                    title: 'Barcha kinolar',
+                    name: 'all'
+                },
+                {
+                    title: 'Mashhur kinolar',
+                    name: 'popular'
+                },
+                {
+                    title: 'Eng ko\'p ko\'rilgan kinolar',
+                    name: 'mostViewrs'
+                },
+            ],
+            filter: 'all'
+        }
+    },
+    methods: {
+        filterHandler(filter) {
+            this.filter = filter
+            this.updateFilterHandler(this.filter)
+        }
     }
-   },
-   data(){
-    return {
-        filter:'all'
-    }
-   },
-   methods:{
-    filterHandler(filter){
-        this.filter = filter
-        this.updateFilterHandler(this.filter)
-    }
-   }
 }
 </script>
 <style ></style>
